819a8405d950f781f4199e4f6a4b8c333a4aa69c
Reiven Cabate <reivenjimmuelfcabate@iskolarngbayan.pup.edu.ph>
2021-11-16 18:48:41 +0800

    Second commit on 1st Program. Added some options within the program together with other conditions.

--- 1stProgram.py
+++ 1stProgram.py
@@ -38,11 +38,31 @@ def evaluate(grade):
     elif grade >= 65 and grade <= 74:
         print('Grade/Mark: 5.0')
         print('Description: Failure')
-
-
+    else:
+        print('Invalid grade percentage. Please input grade percentage from 65 to 100 only.')
+        
 import math
 
-gradeInput = float(input('Insert grade: '))
-gradeRounded = math.ceil(gradeInput)
+print('Welcome to PUP Grading System!')
+print('Kinly follow the instructions to evaluate your grade percentage.')
 
-evaluate(gradeInput)
+studentStatus = input('Before you proceed, are you either incomplete, withdrawn, or dropped? Type YES if you are. If not, type NO: ').upper()
+if studentStatus == 'YES':
+    status = input('Insert your current status: ').upper()
+    if status == 'INCOMPLETE':
+        print('Grade/Mark: Inc.')
+        print('Description: Incomplete')
+    elif status == 'WITHDRAWN':
+        print('Grade/Mark: W')
+        print('Description: WITHDRAWN')
+    elif status == 'DROPPED':
+        print('Grade/Mark: D')
+        print('Description: Dropped')
+    else:
+        print('Invalid status. Please input either INCOMPLETE, WITHDRAWN, or DROPPED.')
+elif studentStatus == 'NO':
+    gradeInput = float(input('Insert your grade percentage: '))
+    gradeRounded = math.ceil(gradeInput)
+    evaluate(gradeRounded)
+else:
+    print('Invalid input. Please input either YES or NO.')
